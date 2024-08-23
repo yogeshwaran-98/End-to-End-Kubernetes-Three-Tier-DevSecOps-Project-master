b1cdcce009b730cbed7fd61daab481c8777d33e0
yogeshwaran-98 <151889778+yogeshwaran-98@users.noreply.github.com>
2024-08-23 16:38:43 +0530

    Delete End-to-End-Kubernetes-Three-Tier-DevSecOps-Project-master directory

--- End-to-End-Kubernetes-Three-Tier-DevSecOps-Project-master/Application-Code/backend/middlewares/errorHandler.js
+++
@@ -1,6 +0,0 @@
-const errorHandler = (error, req, res, next) => {
-  const statusCode = res.statusCode ? res.statusCode : 500;
-  return res.status(statusCode).json({ message: error.message });
-};
-
-module.exports = errorHandler;
